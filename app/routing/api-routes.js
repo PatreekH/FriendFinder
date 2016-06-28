c9b7ca7ee60d5196a59433e42d0d6fb8cf683fbc
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-06-28 01:51:15 -0400

    html/routes

---
+++ app/routing/api-routes.js
@@ -0,0 +1,23 @@
+var friendsData = require('../data/friends.js');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+module.exports = function(app){
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.text());
+	app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+	// API GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
+	// ---------------------------------------------------------------------------
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendsData);
+	});
+
+
+}
--- server.js
+++ server.js
@@ -2,3 +2,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+//==Sets up external routes==
+app.use(express.static(path.join(__dirname, 'app', 'public')));
+
+//==Sets up the Express app to handle data parsing== 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//==Sets up external routes==
+app.use(express.static(path.join(__dirname, 'app', 'public')));
+
+//==Starts the server to begin listening==
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+})

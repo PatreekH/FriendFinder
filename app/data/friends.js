34e65d2b2f50b6d2cd1932e3aa40e19a88b13bfe
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-06-30 02:17:38 -0400

    finished comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,6 +8,31 @@ var friendData = [
 		userName: "Saint Pablo",
 		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
 		results: [3, 5, 3, 2, 1, 4, 5, 2, 3, 1]
+	},
+	{
+		userName: "Bob",
+		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
+		results: [3, 5, 3, 2, 5, 4, 5, 4, 3, 1]
+	},
+	{
+		userName: "Angelica",
+		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
+		results: [4, 3, 3, 5, 2, 3, 1, 2, 2, 4]
+	},
+	{
+		userName: "Jim",
+		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
+		results: [1, 4, 2, 3, 5, 4, 2, 2, 2, 2]
+	},
+	{
+		userName: "Shelby",
+		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
+		results: [3, 5, 2, 1, 3, 2, 4, 3, 5, 3]
+	},
+	{
+		userName: "David",
+		photo: "http://www.telegraph.co.uk/content/dam/music/2016/02/14/kanye-west-lead_1-large_trans++mDlFMJ5bmIWt0sYAakZCiDoJJx60e-G9rsN0orlsZBs.jpg",
+		results: [4, 1, 2, 3, 1, 4, 2, 5, 2, 2]
 	}
 ];
 
